6533b8a3ac90c546ba4009d227cfcdd396a72c17
Abhishek Chopra <40965777+abhishekchopra13@users.noreply.github.com>
2019-12-02 14:14:59 +0530

    Update disjoint_set_union.cpp

--- disjoint_set_union.cpp
+++ disjoint_set_union.cpp
@@ -1,20 +1,7 @@
-// Author -> darklight13
 #define _CRT_SECURE_NO_DEPRECATE
 #include<bits/stdc++.h>
 using namespace std;
-#define int long long int
-typedef long double ld;
-typedef pair<int,int> pii;
-typedef pair<pii,int> piii;
 #define SYNC std::ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
-#define FRE freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
-#define deb(x)     cout << (#x) << " is " << (x) << endl
-#define  pb push_back
-#define  F  first
-#define  S  second
-#define all(v) (v).begin(),(v).end()
-#define sz(a) (int)((a).size())
-#define sq(x) ((x)*(x))
 const int MOD = 1e9+7;
 const int INF = 1000111000111000111LL;
 const int N = 1013;
