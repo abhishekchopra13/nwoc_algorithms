63097f58e7fbf70a565b99cf1032d1a0da9e8b60
Vishal Bhola <38592928+VersatileVishal@users.noreply.github.com>
2019-12-14 19:41:28 +0530

    Update Kadane's Algorithm.cpp

--- Algorithms/DynamicProgramming(DP)/Kadane's Algorithm.cpp
+++ Algorithms/DynamicProgramming(DP)/Kadane's Algorithm.cpp
@@ -1,3 +1,20 @@
+// Kadane's Algorithm
+// Language Used: C++
+// Kadane's algorithm to find maximum sum of the contiguous sub-array in linear time complexity
+// Input Format: The first line of input contains an integer T denoting the number of test cases.The first line of each test case contains a single integer N denoting the size of array.
+//               The second line contains N space-separated integers A1, A2, ..., AN denoting the elements of the array.
+// Output Format: Print the maximum sum of the contiguous sub-array in a separate line for each test case.
+
+// Sample Input: 2    -> Represent no of test cases.
+//               5    		
+//  		 1 2 3 -2 5
+//		 4
+// 		-1 -2 -3 -4
+
+// Sample Output:  9
+//		  -1
+
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -20,17 +37,17 @@ int kadanes(int arr[],int a)
 }
 
 int main() {
-	int t;
-	cin>>t;;
-	while(t--)
+	int testcase;
+	cin>>testcase;
+	while(testcase--)
 	{
-	    int a;
-	    cin>>a;
-	    int arr[a];
-	    for(int i=0;i<a;i++)
+	    int size;
+	    cin>>size;
+	    int arr[size];
+	    for(int i=0;i<size;i++)
 	    cin>>arr[i];
 	    
-	    cout<<kadanes(arr,a)<<endl;
+	    cout<<kadanes(arr,size)<<endl;
 	}
 	return 0;
 }
