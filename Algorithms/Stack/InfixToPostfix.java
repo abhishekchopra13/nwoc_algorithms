cbed8027890b24b519ee50cf1c341b0abe74bf9f
Vishal Bhola <38592928+VersatileVishal@users.noreply.github.com>
2019-12-14 20:12:26 +0530

    Update InfixToPostfix.java

--- Algorithms/Stack/InfixToPostfix.java
+++ Algorithms/Stack/InfixToPostfix.java
@@ -1,9 +1,29 @@
+// InfixToPostfix Conversion
+// Language Used: JAVA
+
+// Given an infix expression in the form of a string str. Convert this infix expression to postfix expression.
+// Infix expression: The expression of the form a op b. When an operator is in-between every pair of operands.
+// Postfix expression: The expression of the form a b op. When an operator is followed for every pair of operands.
+
+// Input format: The first line of input contains an integer T denoting the number of test cases. The next T lines contains an infix expression.
+//		 The expression contains all characters and ^,*,/,+,-.
+// Output format: For each testcase, in a new line, output the infix expression to postfix expression.
+
+//  Sample Input:
+//  2                         Denoting no. of test cases.
+//  a+b*(c^d-e)^(f+g*h)-i
+//  A*(B+C)/D
+
+//  Sample Output:
+//  abcd^e-fgh*+^*+i-
+//  ABC+*D/
+
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
+class InfixToPostfix{
     
     public static int precision(char a)
     {
@@ -29,6 +49,7 @@ class GFG {
     	{
     	    String infix = scan.next();
     	    infix+=")";
+		
     	    Stack<Character>stack = new Stack<Character>();
     	    stack.push('(');
     	    
