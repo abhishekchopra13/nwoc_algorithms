1d8f614ec99497c2a99e3f575d2bafafd949dc4b
vanshita25 <50845855+vanshita25@users.noreply.github.com>
2019-12-19 12:43:26 +0530

    Add files via upload

--- towerofhanoi.c
+++ towerofhanoi.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
-void TowerOfHanoi(int SIZE, char c1, char c2, char c3)
+void TowerOfHanoi(int size, char c1, char c2, char c3)
 {
     if(SIZE==1)
     {
-        printf("%d  Disc  transferred from %c to %c \n", SIZE, c1, c3);
+        printf("%d  Disc  transferred from %c to %c \n", size, c1, c3);
     }
     else{
-        TowerOfHanoi(SIZE-1,c1,c3,c2);
-        printf("%d Transfer Disc From %c TO %c\n",SIZE,c1,c3);
-        TowerOfHanoi(SIZE-1,c2,c1,c3);
+        TowerOfHanoi(size-1,c1,c3,c2);
+        printf("%d Transfer Disc From %c TO %c\n",size,c1,c3);
+        TowerOfHanoi(size-1,c2,c1,c3);
     }
 }
 void main()
