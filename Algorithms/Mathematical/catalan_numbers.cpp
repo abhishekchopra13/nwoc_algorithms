b7b810b90db3c69d490cf8a4447bf0539b163518
Yash Agarwal <38498671+yashagarwal1999@users.noreply.github.com>
2019-12-21 21:03:26 +0530

    Removed single variables.

--- Algorithms/Mathematical/catalan_numbers.cpp
+++ Algorithms/Mathematical/catalan_numbers.cpp
@@ -6,7 +6,7 @@ The Input takes an integer N and the output is the Nth Catalan Number.
 Catalan Numbers are (2nCn)/(n+1) where C is thee binomial coefficient.
 These are special types of numbers which denotes the pattern in many competitive coding contests.
 These numbers were found by Belgian mathematician Catalan.
-In Input give which Catalan number you want to find.
+In Input give which Catalan number you want to find. 
 The function catalan_number computes it and returns the result which is printed.
 For more information on Catalan Numbers:https://en.wikipedia.org/wiki/Catalan_number
 */
@@ -23,9 +23,9 @@ using namespace std;
 
 const int MAX=1000000;
 ll catalan[MAX];
-ll catalan_number(ll n) {
+ll catalan_number(ll catalan_number_to_be_found) {
     catalan[0] = catalan[1] = 1;
-    for (int i=2; i<=n; i++) {
+    for (int i=2; i<=catalan_number_to_be_found; i++) {
         catalan[i] = 0;
         for (int j=0; j < i; j++) {
             catalan[i] += (catalan[j] * catalan[i-j-1]) % MOD;
@@ -34,17 +34,17 @@ ll catalan_number(ll n) {
             }
         }
     }
-    return catalan[n];
+    return catalan[catalan_number_to_be_found];
 }
 
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);cout.tie(0);
-ll n;
-cin>>n;
-ll x=catalan_number(n);
-cout<<x<<endl;
+ll catalan_number_to_be_found;
+cin>>catalan_number_to_be_found;
+ll answer=catalan_number(catalan_number_to_be_found);
+cout<<answer<<endl;
 
   return 0;
     
