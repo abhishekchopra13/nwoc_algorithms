c457f4071afbba0f916686b83dafebd321f8e2ba
Yash Agarwal <38498671+yashagarwal1999@users.noreply.github.com>
2019-12-21 12:34:21 +0530

    Added Sample Input/Output  with Algorithm Name

--- Algorithms/Mathematical/fast_power.cpp
+++ Algorithms/Mathematical/fast_power.cpp
@@ -1,3 +1,5 @@
+//Algorithm Name: Binary Exponentiation 
+
 #include<bits/stdc++.h>
 #define MOD 1000000007
 #define MOD9 1000000009
@@ -26,14 +28,21 @@ int main(){
  ll a,b,c;
  cin>>a>>b>>c;
  ll ans=fast_power(a,b,c);
- cout<<ans<<endl;
- 
-
-
-
-    
-    
-    
+ cout<<ans<<endl; 
   return 0;
     
 }
+
+/*
+Sample Input:
+2 7 5
+Sample Output:
+3
+
+Explanation:
+The Input is 3 integers a,b,c.
+The ouput is  (a raised to b)mod c.
+Therefore 2^7=128 and 128%5=3.
+We want to find (a raised to b ) mod c in logN time where N is the number of the bits in the binary  representation of b. The algorithm is called as Binary Exponentiation which uses the binary representation of b to compute the result in O(logN) time instead of O(b).
+The details of the algorithm can be found at: https://www.geeksforgeeks.org/modular-exponentiation-power-in-modular-arithmetic/
+ */
