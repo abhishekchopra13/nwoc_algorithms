43f0dfd467ae144b4fe074ae28b00c6ff2fd0c80
Shiv Sharma <shivsharmavictory@gmail.com>
2019-12-30 13:28:36 +0530

    LCM

--- Algorithms/mathematical_algorithm/LCM.cpp
+++ Algorithms/mathematical_algorithm/LCM.cpp
@@ -1,24 +1,27 @@
 //LCM of two numbers is the smallest number which can be divided by both numbers
 // a*b=LCM(a,b) * GCD(a,b)
+//C++ program for LCM
 #include<bits/stdc++.h>
 using namespace std;
-int gcd(int a,int b)
+int gcd(int number1,int number2)
 {
-	if(b==0)
-	return a;
-	return gcd(b,a%b);
+	if(number2==0)
+	return number1;
+	return gcd(number2,number1%number2);
 }
-int LCM(int a,int b)
+int LCM(int number1,int number2)
 {
-	return (a*b)/gcd(a,b);
+	return (number1*number2)/gcd(number1,number2);
 }
+
+//Driver Code
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(); cout.tie(NULL);
-	int a,b;
+	int number1,number2;
 	cout<<"Enter the both number's:"<<endl;
-	cin>>a>>b;
-	cout<<LCM(a,b)<<endl;
+	cin>>number1>>number2;
+	cout<<LCM(number1,number2)<<endl;
 	return 0;
 }
