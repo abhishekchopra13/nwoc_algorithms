467fedd2d9a9394c9351eb8da3f0e2683014252b
Shiv Sharma <shivsharmavictory@gmail.com>
2019-12-30 13:38:32 +0530

    FACTORIZATION

--- Algorithms/mathematical_algorithm/Factorization.cpp
+++ Algorithms/mathematical_algorithm/Factorization.cpp
@@ -1,31 +1,33 @@
 //Given an number n, write an efficient function to print all prime factor of n
+
+//C++ program for FACTORIZATION
 #include<bits/stdc++.h>
 using namespace std;
-void primeFactor(int n)
+void primeFactor(int number)
 {
-	while(n%2==0)
+	while(number%2==0)
 	{
-		n=n/2;
+		number=number/2;
 		cout<<"2"<<" ";
 	}
-	for(int i=3;i<=sqrt(n);i=i+2)
+	for(int i=3;i<=sqrt(number);i=i+2)
 	{
-		while(n%i==0)
+		while(number%i==0)
 		{
 			cout<<i<<" ";
-			n=n/i;
+			number=number/i;
 		}
 	}
-	if(n>2)
-	cout<<n<<" ";
+	if(number>2)
+	cout<<number<<" ";
 }
 int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int n;
+	int number;
 	cout<<"Enter the Number:"<<endl;
-	cin>>n;
-	primeFactor(n);
+	cin>>number;
+	primeFactor(number);
 	return 0;
 }
