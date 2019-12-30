d40c890dadf48d8674b5748fb1534eba49675697
Shiv Sharma <shivsharmavictory@gmail.com>
2019-12-30 13:42:27 +0530

    EULER'S TOTIENT FUNCTION

--- Algorithms/mathematical_algorithm/Euler's_Totient_function.cpp
+++ Algorithms/mathematical_algorithm/Euler's_Totient_function.cpp
@@ -1,19 +1,21 @@
 //Euler's toient function ?(n) for an input n is count of numbers in {1,2,3,......n} that are relatively prime to n
 // i.e., the number whose GCD with n is 1
+
+//C++ program for EULER'S TOTIENT FUNCTION
 #include<bits/stdc++.h>
 using namespace std;
-int gcd(int a,int b)
+int gcd(int i,int number)
 {
-	if(b==0)
-	return a;
-	return gcd(b,a%b);
+	if(number==0)
+	return i;
+	return gcd(number,i%number);
 }
-int fun(int n)
+int fun(int number)
 {
 	int result=0;
-	for(int i=1;i<n;i++)
+	for(int i=1;i<number;i++)
 	{
-		if(gcd(i,n)==1)
+		if(gcd(i,number)==1)
 		{
 			result++;
 		}
@@ -24,9 +26,9 @@ int main()
 {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int n;
+	int number;
 	cout<<"Enter the number:"<<endl;
-	cin>>n;
-	cout<<fun(n)<<endl;
+	cin>>number;
+	cout<<fun(number)<<endl;
 	return 0;
 }
