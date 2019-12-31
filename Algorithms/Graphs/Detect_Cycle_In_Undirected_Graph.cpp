2a54d9b168dd65d760b3b33e6044956c5c26be1b
Akash Kumar Singhal <41924636+akashksinghal@users.noreply.github.com>
2019-12-31 15:07:52 +0530

    Update Detect_Cycle_In_Undirected_Graph.cpp
    Updated the advised changes

--- Algorithms/Graphs/Detect_Cycle_In_Undirected_Graph.cpp
+++ Algorithms/Graphs/Detect_Cycle_In_Undirected_Graph.cpp
@@ -8,18 +8,17 @@ void addEdge(vector<int> adj[], int u, int v)
 	adj[v].push_back(u); 
 } 
 
-bool isCyclicConntected(vector<int> adj[], int s, 
-						int V, vector<bool>& visited) 
+bool isCyclicConntected(vector<int> adj[], int VertexToBeVisited,int NoOfVertices, vector<bool>& visited) 
 { 
 	// Set parent vertex for every vertex as -1. 
-	vector<int> parent(V, -1); 
+	vector<int> parent(NoOfVertices, -1); 
 
 	// Create a queue for BFS 
 	queue<int> q; 
 
 	// Mark the current node as visited and enqueue it 
-	visited[s] = true; 
-	q.push(s); 
+	visited[VertexToBeVisited] = true; 
+	q.push(VertexToBeVisited); 
 
 	while (!q.empty()) { 
 
@@ -27,8 +26,10 @@ bool isCyclicConntected(vector<int> adj[], int s,
 		int u = q.front(); 
 		q.pop(); 
 
-		for (auto v : adj[u]) { 
-			if (!visited[v]) { 
+		for (auto v : adj[u]) 
+		{ 
+			if (!visited[v]) 
+			{ 
 				visited[v] = true; 
 				q.push(v); 
 				parent[v] = u; 
@@ -44,10 +45,8 @@ bool isCyclicDisconntected(vector<int> adj[], int V)
 { 
 	// Mark all the vertices as not visited 
 	vector<bool> visited(V, false); 
-
 	for (int i = 0; i < V; i++) 
-		if (!visited[i] && isCyclicConntected(adj, i, 
-										V, visited)) 
+		if (!visited[i] && isCyclicConntected(adj, i,V, visited)) 
 			return true; 
 	return false; 
 } 
@@ -55,13 +54,23 @@ bool isCyclicDisconntected(vector<int> adj[], int V)
 // Driver program to test methods of graph class 
 int main() 
 { 
-	int V = 4; 
+	cout<<"enter the number of vertices : ";
+	int V ;//V stands for Number of Verticies
+	cin>>V;
+	//adj[V] is 2D vector to store the connection between 2 vertices
+	int vertex1,vertex2;
 	vector<int> adj[V]; 
-	addEdge(adj, 0, 1); 
-	addEdge(adj, 1, 2); 
-	addEdge(adj, 2, 0); 
-	addEdge(adj, 2, 3); 
-
+	do{
+		char ch;
+		cout<<"Enter Edge Details :\n";
+		cout<<"Enter Vertex 1 : ";
+		cin>>vertex1;
+		cout<<"Enter Vertex 2 : ";
+		cin>>vertex2;
+		addEdge(adj,vertex1,vertex2);
+		cout<<"wanna add more edges(y/n):";
+		cin>>ch;
+	}while(ch!='y');
 	if (isCyclicDisconntected(adj, V)) 
 		cout << "Yes"; 
 	else
